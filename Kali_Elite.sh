c133ca08c8af74a95974526b4a734b204f8afb6c
Afshin Fakhim <74267429+a9v8i@users.noreply.github.com>
2024-02-13 03:45:41 +0330

    Update Kali_Elite.sh

--- Kali_Elite.sh
+++ Kali_Elite.sh
@@ -3401,7 +3401,7 @@ main ()
 	apt update;apt upgrade -qy;apt dist-upgrade -qy
 
 	# Install Requirement Tools
-	apt install -qy curl git gnupg apt-transport-https tor obfs4proxy docker.io docker-compose python3 nodejs npm cargo golang python2 libreoffice vlc uget remmina openconnect bleachbit powershell filezilla telegram-desktop joplin thunderbird mono-complete mono-devel node-ws p7zip p7zip-full wine winetricks winbind cmake build-essential binutils net-tools snmp-mibs-downloader locate alacarte imagemagick ghostscript software-properties-common libre2-dev cassandra gnupg2 ca-certificates htop nload gimp cmatrix zipalign ffmpeg rar g++ libssl-dev python3-dev python3-pip python3-poetry guymager libgd-perl libimage-exiftool-perl libstring-crc32-perl nuget nim rustup musl-tools 
+	apt install -qy curl git gnupg apt-transport-https tor obfs4proxy docker.io docker-compose python3 nodejs npm cargo golang python2 libreoffice vlc uget remmina openconnect bleachbit powershell filezilla thunderbird mono-complete mono-devel node-ws p7zip p7zip-full wine winetricks winbind cmake build-essential binutils net-tools snmp-mibs-downloader locate alacarte imagemagick ghostscript software-properties-common libre2-dev cassandra gnupg2 ca-certificates htop nload gimp cmatrix zipalign ffmpeg rar g++ libssl-dev python3-dev python3-pip python3-poetry guymager libgd-perl libimage-exiftool-perl libstring-crc32-perl nim rustup musl-tools 
 
 	# Install Python2 pip
 	wget https://bootstrap.pypa.io/pip/2.7/get-pip.py -O /tmp/get-pip.py;python2.7 /tmp/get-pip.py;rm -f /tmp/get-pip.py
