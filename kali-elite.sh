44a9a39a7df181e39d03ab18b7e0316dbd00df4a
Afshin Fakhim <74267429+a9v8i@users.noreply.github.com>
2024-04-14 18:02:12 +0330

    Update kali-elite.sh

--- kali-elite.sh
+++ kali-elite.sh
@@ -2840,11 +2840,11 @@ EOF
 	fi
 
 	# Install FRP
-	if [ ! -d "/usr/share/frp_0.55.1_linux_amd64" ]; then
+	if [ ! -d "/usr/share/frp_*_linux_amd64" ]; then
 		wget https://github.com/fatedier/frp/releases/latest/download/frp_0.57.0_linux_amd64.tar.gz  -O /tmp/frp_linux-amd64.tar.gz
 		tar -xvf /tmp/frp_linux-amd64.tar.gz -C /usr/share;rm -f /tmp/frp_linux-amd64.tar.gz
-		chmod 755 /usr/share/frp_0.55.1_linux_amd64/*
-		ln -fs /usr/share/frp_0.55.1_linux_amd64/frps /usr/bin/frps
+		chmod 755 /usr/share/frp_*_linux_amd64/*
+		ln -fs /usr/share/frp_*_linux_amd64/frps /usr/bin/frps
 		chmod +x /usr/bin/frps
 		menu_entry "Exfiltration" "Red-Team" "FRP" "/usr/share/kali-menu/exec-in-shell 'frp -h'"
 		printf "$GREEN"  "[*] Success Installing FRP"
