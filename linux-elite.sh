5725e13ae49bddd266a5440e1f0c2a0964019785
Afshin Fakhim <74267429+a9v8i@users.noreply.github.com>
2024-08-18 23:12:20 +0330

    Update linux-elite.sh

--- linux-elite.sh
+++ linux-elite.sh
@@ -1503,7 +1503,7 @@ EOF
 		wget https://github.com/xvzc/SpoofDPI/releases/latest/download/spoof-dpi-linux-amd64.tar.gz -O /tmp/$name.tar.gz
 		tar -xvf /tmp/$name.tar.gz -C /usr/share/$name;rm -f /tmp/$name.tar.gz
 		chmod 755 /usr/share/$name/*
-		ln -fs /usr/share/$name/donut /usr/bin/$Donut
+		ln -fs /usr/share/$name/spoof-dpi /usr/bin/$name
 		chmod +x /usr/bin/$name
 		menu_entry "Network" "Penetration-Testing" "$name" "$exec_shell '$name -h'"
 		printf "$GREEN"  "[*] Success installing $name"
@@ -2102,7 +2102,7 @@ EOF
 		wget https://github.com/TheWover/donut/releases/latest/download/donut_v1.0.tar.gz -O /tmp/$name.tar.gz
 		tar -xvf /tmp/$name.tar.gz -C /usr/share/$name;rm -f /tmp/$name.tar.gz
 		chmod 755 /usr/share/$name/*
-		ln -fs /usr/share/$name/donut /usr/bin/$Donut
+		ln -fs /usr/share/$name/donut /usr/bin/$name
 		chmod +x /usr/bin/$name
 		menu_entry "Execution" "Red-Team" "$name" "$exec_shell '$name -h'"
 		printf "$GREEN"  "[*] Success installing $name"
