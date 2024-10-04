38559061270a3de1196d50e9f3122202c7368d61
Afshin Fakhim <74267429+a9v8i@users.noreply.github.com>
2024-10-04 11:18:22 +0330

    Update linux-elite.sh

--- linux-elite.sh
+++ linux-elite.sh
@@ -1348,7 +1348,7 @@ EOF
 	apt install -qy awscli trivy 
 
 	# install Python3 pip
-	cloud_pip="sceptre aclpwn powerpwn ggshield pacu whispers s3scanner roadrecon roadlib gcp_scanner roadtx festin cloudsplaining c7n trailscraper lambdaguard airiam access-undenied-aws n0s1 aws-gate cloudscraper acltoolkit-ad prowler bloodhound aiodnsbrute gorilla-cli knowsmore checkov scoutsuite endgame timberlake punch-q"
+	cloud_pip="sceptre aclpwn cloudshovel powerpwn ggshield pacu whispers s3scanner roadrecon roadlib gcp_scanner roadtx festin cloudsplaining c7n trailscraper lambdaguard airiam access-undenied-aws n0s1 aws-gate cloudscraper acltoolkit-ad prowler bloodhound aiodnsbrute gorilla-cli knowsmore checkov scoutsuite endgame timberlake punch-q"
 	pip_installer "Cloud" "Penetration-Testing" "$cloud_pip"
 
 	# install Nodejs NPM
