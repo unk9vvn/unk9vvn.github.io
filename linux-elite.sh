646d2e0bea303e83b5b8b17ac130547e8692b6ee
Afshin Fakhim <74267429+a9v8i@users.noreply.github.com>
2025-02-16 17:22:18 +0330

    Update linux-elite.sh

--- linux-elite.sh
+++ linux-elite.sh
@@ -398,6 +398,7 @@ go install github.com/tomnomnom/waybackurls@latest;ln -fs ~/go/bin/waybackurls /
 go install github.com/tomnomnom/httprobe@latest;ln -fs ~/go/bin/httprobe /usr/bin/httprobe
 go install github.com/tomnomnom/meg@latest;ln -fs ~/go/bin/meg /usr/bin/meg
 go install github.com/projectdiscovery/katana/cmd/katana@latest;ln -fs ~/go/bin/katana /usr/bin/katana
+go install github.com/j3ssie/osmedeus@latest;ln -fs ~/go/bin/osmedeus /usr/bin/osmedeus
 go install github.com/edoardottt/cariddi/cmd/cariddi@latest;ln -fs ~/go/bin/cariddi /usr/bin/cariddi
 go install github.com/glebarez/cero@latest;ln -fs ~/go/bin/cero /usr/bin/cero
 go install github.com/1hehaq/recx@latest;ln -fs ~/go/bin/recx /usr/bin/recx
