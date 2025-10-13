8e81b51fa2d3f29c2914520630d564473d6887a9
Afshin Fakhim <74267429+a9v8i@users.noreply.github.com>
2025-10-13 11:57:30 +0330

    Update linux-elite.sh

--- linux-elite.sh
+++ linux-elite.sh
@@ -616,12 +616,27 @@ EOF
 		printf "$GREEN"  "[*] Successfully Installed $name"
 	fi
 
+ 	# install gitdorker
+	if [ ! -d "/usr/share/gitdorker" ]; then
+		name="gitdorker"
+		git clone https://github.com/obheda12/GitDorker /usr/share/$name
+		chmod 755 /usr/share/$name/*
+        pip3 install -r /usr/share/$name/requirements.txt --break-system-packages
+		cat > /usr/bin/$name << EOF
+#!/bin/bash
+cd /usr/share/$name;python3 GitDorker.py "\$@"
+EOF
+		chmod +x /usr/bin/$name
+		menu_entry "Web" "Penetration-Testing" "$name" "$exec_shell '$name'"
+		printf "$GREEN"  "[*] Successfully Installed $name"
+	fi
+
   	# install changeme
 	if [ ! -d "/usr/share/changeme" ]; then
 		name="changeme"
 		git clone https://github.com/ztgrace/changeme /usr/share/$name
 		chmod 755 /usr/share/$name/*
-  		pip3 install -r /usr/share/$name/requirements.txt
+        pip3 install -r /usr/share/$name/requirements.txt --break-system-packages
 		cat > /usr/bin/$name << EOF
 #!/bin/bash
 cd /usr/share/$name;python3 changeme.py "\$@"
